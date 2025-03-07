f1aed38aa9df8a7bb77170c914bf4cf27bc084a4
jasirck <muhammedjasirstd@gmail.com>
2025-03-07 18:26:54 +0530

    statis

--- paris/settings.py
+++ paris/settings.py
@@ -174,7 +174,10 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
