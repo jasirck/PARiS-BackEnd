5b95191809436b66f5f8b185ebc91dba13118cc3
jasirck <muhammedjasirstd@gmail.com>
2025-03-12 20:42:53 +0530

    socket fix

--- paris/asgi.py
+++ paris/asgi.py
@@ -1,11 +1,16 @@
 import os
+import django  
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from visas.routing import websocket_urlpatterns as visas_websocket_urlpatterns
-from messege.routing import websocket_urlpatterns as messege_websocket_urlpatterns
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "paris.settings")
+
+django.setup()  
+
+from visas.routing import websocket_urlpatterns as visas_websocket_urlpatterns
+from messege.routing import websocket_urlpatterns as messege_websocket_urlpatterns
 
 application = ProtocolTypeRouter(
     {
