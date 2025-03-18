2fbab7f330ed7cb277f1c4b66d840e49a312454e
jasirck <muhammedjasirstd@gmail.com>
2025-03-18 16:38:38 +0530

    aa

--- packages/views.py
+++ packages/views.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Package, Days, BookedPackage, PackageCategory
+from packages.models import Package, Days, BookedPackage, PackageCategory
 from users.models import User
 from .serializers import (
     PackageSerializer,
