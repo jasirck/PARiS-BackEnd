8af12e3d849e046f224b4350b76a03450216e191
jasirck <muhammedjasirstd@gmail.com>
2025-03-22 12:37:53 +0530

    ci/cd check 10

--- paris/urls.py
+++ paris/urls.py
@@ -23,7 +23,7 @@ def webhook(request):
             expected_signature = hmac.new(secret_token.encode(), body, hashlib.sha256).hexdigest()
 
             if not hmac.compare_digest(signature, expected_signature):
-                return JsonResponse({'error': f"Inv alied signature, Received Signature: {signature},Expected  Signature: {expected_signature}"}, status=403)
+                return JsonResponse({'error': f"Inv al ied signature, Received Signature: {signature},Expected  Signature: {expected_signature}"}, status=403)
 
             # Execute Git pull and restart services
             repo_dir = config("PROJECT_DIR")
