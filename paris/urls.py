11bf51ccd26b2a7c68b87a606e471c92bd195230
jasirck <muhammedjasirstd@gmail.com>
2025-03-22 11:46:31 +0530

    ci/cd

--- paris/urls.py
+++ paris/urls.py
@@ -1,11 +1,42 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from django.http import HttpResponse
-
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import subprocess
+import hmac
+import hashlib
+from decouple import config
+import json
 
 def home(request):
     return HttpResponse("Hello, Django is working!")
+
+@csrf_exempt
+def webhook(request):
+    if request.method == 'POST':
+        try:
+            # Validate signature
+            signature = request.headers.get('X-Hub-Signature-256', '').split('sha256=')[-1].strip()
+            body = request.body
+            secret_token = config("SSL_SECRET_TOKEN")
+            expected_signature = hmac.new(secret_token.encode(), body, hashlib.sha256).hexdigest()
+
+            if not hmac.compare_digest(signature, expected_signature):
+                return JsonResponse({'error': 'Invalid signature'}, status=403)
+
+            # Execute Git pull and restart services
+            repo_dir = config("PROJECT_DIR")
+            subprocess.run(['git', '-C', repo_dir, 'pull', 'origin', 'aws-deployment'])
+            subprocess.run(['sudo', 'systemctl', 'restart', 'gunicorn'])
+            subprocess.run(['sudo', 'systemctl', 'restart', 'nginx'])
+
+            return JsonResponse({'status': 'success'})
+
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=500)
+    
+    return JsonResponse({'error': 'Invalid request method'}, status=400)
 
 
 urlpatterns = [
@@ -21,4 +52,5 @@ urlpatterns = [
     path("api/", include("visas.urls")),
     path("api/", include("flights.urls")),
     path('', home),
+    path('webhook/', webhook, name='webhook'),
 ]
