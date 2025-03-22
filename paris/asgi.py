1022f130d8555e0f4f34b1b77b3eefae0a733d58
jasirck <muhammedjasirstd@gmail.com>
2025-03-22 14:35:49 +0530

    notifications

--- paris/asgi.py
+++ paris/asgi.py
@@ -3,8 +3,6 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from visas.routing import websocket_urlpatterns as visas_websocket_urlpatterns
-from resorts.routing import websocket_urlpatterns as resorts_websocket_urlpatterns
-from packages.routing import websocket_urlpatterns as packages_websocket_urlpatterns
 from messege.routing import websocket_urlpatterns as messege_websocket_urlpatterns
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "paris.settings")
@@ -13,7 +11,7 @@ application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
         "websocket": AuthMiddlewareStack(
-            URLRouter(resorts_websocket_urlpatterns + packages_websocket_urlpatterns + visas_websocket_urlpatterns + messege_websocket_urlpatterns)
+            URLRouter(visas_websocket_urlpatterns + messege_websocket_urlpatterns)
         ),
     }
 )
--- resorts/routing.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import consumers
-
-websocket_urlpatterns = [
-    path("ws/resort_notifications/", consumers.NotificationConsumer.as_asgi()),
-]
